62ba005c67ac9d99dbb51cdd0591c19801a44da4
utsu <mentalsupernovafox@gmail.com>
2023-10-01 18:07:57 +0300

    adding basic structure for project

---
+++ gui/src/App.js
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<h1>Shit</h1>}></Route>
+        <Route path="/about" element={<h1>About</h1>}></Route>
+        <Route path="/contact" element={<h1>Contact</h1>}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
